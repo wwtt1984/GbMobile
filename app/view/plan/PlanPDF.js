17ed213fdb84ca519d6d49f1174f58cec7259e45
teddyBearWang <815819325@qq.com>
2015-05-27 18:52:43 +0800

    Update PlanPDF.js

--- app/view/plan/PlanPDF.js
+++ app/view/plan/PlanPDF.js
@@ -10,6 +10,20 @@ Ext.define('YzMobile.view.plan.PlanPDF', {
         layout:'fit',
         items: [
             {
+                xtype:'toolbar',
+                docked: 'top',
+                ui: 'light',
+                title: '预案浏览',
+                items:[
+                    {
+                        xtype:'button',
+                        text:'返回',
+                        itemId:'pdfbtn',
+                        ui: 'back'
+                    }
+                ]
+            },
+            {
                 xtype: 'panel',
                 html: '<div style="height:100%;-webkit-overflow-scrolling:touch;">' +
                 '<iframe width="100%" height="700px" src="http://115.236.2.245:38028/web/viewer.html?file=/examples/helloworld/helloworld.pdf" frameborder="0" allowfullscreen></iframe>' +
